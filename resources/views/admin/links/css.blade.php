ee87701df9d7b0471eac87fadb3b9e9b23fe0b01
unknown <yeasin18801@gmail.com>
2024-02-02 15:15:51 +0600

    Dashboard Setup

---
+++ resources/views/admin/links/css.blade.php
@@ -0,0 +1,15 @@
+
+<link rel="apple-touch-icon" href="{{ asset('panel') }}/apple-icon.png">
+    <link rel="shortcut icon" href="{{ asset('panel') }}/favicon.ico">
+
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="{{ asset('panel') }}/vendors/jqvmap/dist/jqvmap.min.css">
+
+
+    <link rel="stylesheet" href="{{ asset('panel') }}/assets/css/style.css">
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
